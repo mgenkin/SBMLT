3bad8e61890d54e435063de48ddef7dad4d2d03d
mgenkin <genkin.matt@gmail.com>
2015-11-06 18:22:28 -0500

    barely filled in neural net file

--- neural_network.py
+++ neural_network.py
@@ -1,44 +1,48 @@
-def NN_Node(object):
-	def __init__(self, input_shape, output_shape):
-		self.W 
-		self.b
-
-	def get_output(self, input):
-
-		return output
-
-	def update_gradient(self, cost):
-		# cost may be a list
-		# update W and b to new values
+import numpy as np
 
 
-def NodeLayer(object):
-	def __init__(self, input_shape, output_shape, num_nodes):
-		self.nodes = # a list of nodes, initialized with the proper shapes
+class NN_Node(object):
 
-	def get_output(self, input):
-		# compute the output of the layer
-		return output
+    def __init__(self, input_shape):
+        self.W = np.random.randarray(input_shape)
+        self.b = np.random.random()
 
-	def compute_gradient(self, cost):
-		# cost may be a list of costs from the neurons of the next layer
+    def get_output(self, node_input):
+    	output = self.W*node_input+self.b
+        return output
+
+    def update_gradient(self, cost):
+        # cost may be a list
+        # update W and b to new values
 
 
-def NeuralNetwork(object):
-	
-	def __init__(self, in_size, layer_sizes, out_size):
+class NodeLayer(object):
 
-	def feedforward_output(self, input):
+    def __init__(self, input_shape, output_shape, num_nodes):
+        self.nodes =  # a list of nodes, initialized with the proper shapes
 
-		return output
+    def get_output(self, input):
+        # compute the output of the layer
+        return output
 
-	def update_gradient(self, cost):
-		# implement backpropagation through the layers
+    def compute_gradient(self, cost):
+        # cost may be a list of costs from the neurons of the next layer
 
+
+class NeuralNetwork(object):
+
+    def __init__(self, in_size, layer_sizes, out_size):
+
+    def feedforward_output(self, input):
+
+        return output
+
+    def update_gradient(self, cost):
+        # implement backpropagation through the layers
 
 
 nn = NeuralNetwork(10, [100], 10)
 for i in range(500):
-	y_pred = nn.feedforward_output(data)
-	cost = (y_true - y_pred)**2
-	nn.update_gradient(cost)
+    y_pred = nn.feedforward_output(data)
+    cost = (y_true - y_pred)**2
+    nn.update_gradient(cost)
